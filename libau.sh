0117a8d0146297303c740e029400fa5d1501bbdb
andrew-llcoder <124795152+andrew-llcoder@users.noreply.github.com>
2023-03-14 08:52:21 +0200

    Update libau.sh

--- libau.sh
+++ libau.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+get_file(){
+    echo Downloading `basename $1`...
+    wget $1 -c --directory-prefix=$2 -nc -q --show-progress
+}
+
+create_r_folders(){
+    r_folders=`curl -L https://raw.githubusercontent.com/andrew-llcoder/AurumOS/main/r_folders`
+    for dir in $r_folders
+    do
+        mkdir -p $r_folders
+    done
+}
+
 unpack_tar(){
     echo Unpacking $1...
     tar -xf $1
